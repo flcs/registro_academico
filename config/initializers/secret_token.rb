fd891398b37f4873e0079fee2ea602a3bb87742a
Natanael de Araújo <internael@gmail.com>
2012-02-17 17:26:53 -0200

    projeto vazio com railsadmin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Registro::Application.config.secret_token = '10ef8db4827e90a9ffa382b365d646664b75338b934ee6230f929ab7e6f2ca7c19241a0099ade93324b8e9f4f793b4a42b552dc9cfaffbc5b43c8b8166c94b72'
